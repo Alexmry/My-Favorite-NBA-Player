d789dbad613b5a1fe11f08a29a46143e9c5c4046
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-28 16:43:04 +0000

    Added the scroll up arrow in js file

--- src/app.js
+++ src/app.js
@@ -53,8 +53,4 @@ search.addWidgets([
 search.start();
 
 
-function scrollTop(){
-  let scrollTop = document.getElementById('scroll-top')
-  if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollTop)
+
